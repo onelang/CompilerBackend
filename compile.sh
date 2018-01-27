6ad5284e7488326c2753e5b89b52b494b738347a
KT <koczkatamas@gmail.com>
2018-01-27 18:39:07 +0100

    compile.sh: stop on error, "sh" doesn't like pushd / popd

--- compile.sh
+++ compile.sh
@@ -1,18 +1,19 @@
+set -e
 echo
 echo '    ===> Building Java in-memory compiler <==='
 echo
-pushd InMemoryCompilers/Java > /dev/null
+cd InMemoryCompilers/Java
 mvn compile
 mkdir lib/ 2> /dev/null
 cp ~/.m2/repository/com/google/code/gson/gson/2.8.1/gson-2.8.1.jar lib/
-popd > /dev/null
+cd ../../
 
 echo
 echo '    ===> Building C# in-memory compiler <==='
 echo
-pushd InMemoryCompilers/CSharp > /dev/null
+cd InMemoryCompilers/CSharp
 dotnet build
-popd > /dev/null
+cd ../../
 
 echo
 echo '    ===> Installing npm dependencies <==='
