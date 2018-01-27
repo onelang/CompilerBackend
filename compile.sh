8dc37e3451f14eddb7cedc2f6c8470d90b2007c5
KT <koczkatamas@gmail.com>
2018-01-27 16:00:52 +0100

    Initial commit (moved from onelang repo)

---
+++ compile.sh
@@ -0,0 +1,15 @@
+echo
+echo '    ===> Building Java in-memory compiler <==='
+echo
+pushd InMemoryCompilers/Java > /dev/null
+mvn compile
+mkdir lib/ 2> /dev/null
+cp ~/.m2/repository/com/google/code/gson/gson/2.8.1/gson-2.8.1.jar lib/
+popd > /dev/null
+
+echo
+echo '    ===> Building C# in-memory compiler <==='
+echo
+pushd InMemoryCompilers/CSharp > /dev/null
+dotnet build
+popd > /dev/null
