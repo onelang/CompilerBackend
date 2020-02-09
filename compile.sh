26cf40b3a45f0631e6484599bfb6f185dcd984b0
KT <koczkatamas@gmail.com>
2020-02-09 03:40:31 +0100

    Fix build break if lib folder exists

--- compile.sh
+++ compile.sh
@@ -4,7 +4,7 @@ echo '    ===> Building Java in-memory compiler <==='
 echo
 cd InMemoryCompilers/Java
 mvn compile
-mkdir lib/ 2> /dev/null
+mkdir lib/ 2> /dev/null && exit 0
 cp ~/.m2/repository/com/google/code/gson/gson/2.8.1/gson-2.8.1.jar lib/
 cd ../../
 
